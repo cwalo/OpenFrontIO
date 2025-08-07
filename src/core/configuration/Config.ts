f11d007d80b27b15823748ad83b45ed09975fa27
Aleksey Orekhovsky <aaa4xu@gmail.com>
2025-08-07 13:49:58 +0700

    Pull replay files from api

--- src/core/configuration/Config.ts
+++ src/core/configuration/Config.ts
@@ -62,6 +62,7 @@ export type ServerConfig = {
   cloudflareCredsPath(): string;
   stripePublishableKey(): string;
   allowedFlares(): string[] | undefined;
+  replayFallbackUrl(gameId: GameID): string;
 };
 
 export type NukeMagnitude = {
